f331d8f41026ec848c110007b03d3ab693f68f90
tum009 <tumpabanik652@gmail.com>
2025-06-02 15:24:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎Bot',
   botName: "Knight Bot",
   botOwner: 'Debjit', // Your name
-  ownerNumber: '917439160237', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '7439160237', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
